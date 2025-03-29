2b044bb2d0f3e097c2774be6f758030b896fb019
eak300515 <eakadityakumar15@gmail.com>
2025-03-29 13:42:38 +0530

    BUG FIXING

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -36,7 +36,7 @@ const Home = () => {
         initial={{ opacity: 0, y: -50 }} 
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1 }}>
-        C O D E A s T R A A !
+        CODE ASTRAA
       </motion.h1>
 
       <motion.h6 
