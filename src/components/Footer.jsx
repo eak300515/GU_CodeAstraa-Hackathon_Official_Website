232ead0e4dc099eb132e4f9d1aa257e707ea3170
eak300515 <eakadityakumar15@gmail.com>
2025-03-29 14:02:12 +0530

    Finalizing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import "./../styles/Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>© 2025 CodeAstraa - Created by Wizards -
+      <p>© 2025 CODE ASTRAA - CREATED BY WIZARDS -
       GALGOTIAS TECH COUNCIL
       </p>
     </footer>
