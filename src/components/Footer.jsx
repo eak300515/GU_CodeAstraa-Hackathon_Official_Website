0e785f03d92c552cf9e15bb1d2accb4964a939b4
eak300515 <eakadityakumar15@gmail.com>
2025-03-29 14:00:04 +0530

    Footer Edit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,9 @@ import "./../styles/Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>© 2025 CodeAstraa - Created by Wizards</p>
+      <p>© 2025 CodeAstraa - Created by Wizards -
+      GALGOTIAS TECH COUNCIL
+      </p>
     </footer>
   );
 };
